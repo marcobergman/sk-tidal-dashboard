73e9d18b80cf5f04aeab1acc92ccf8d78d3f915a
Marco Bergman <marcobergman@gmail.com>
2022-03-29 00:05:46 +0200

    improved target path

--- postInstall.js
+++ postInstall.js
@@ -1,15 +1,18 @@
 
 const fs = require("fs");
+const path = require("path");
 
-const TARGET = "/home/pi/.signalk/plugin-config-data/sk-tidal-dashboard.json";
+const TARGET = path.resolve (__dirname + '/../../plugin-config-data/sk-tidal-dashboard.json');
+
 const SOURCE = "sk-tidal-dashboard.json";
 
 if (fs.existsSync(TARGET)) {
-  console.log(TARGET, "exists");
+  console.log(TARGET, "exists, no action");
 } else {
   console.log(TARGET, "does not exist");
   fs.copyFile(SOURCE, TARGET, (err) => {
     if (err) throw err;
-    console.log(SOURCE, "was copied to", TARGET);
+    console.log("Template file", SOURCE, "was copied to", TARGET);
   });
 }
+
